10126ed46b33a2931dd100191c9c08923a9289d1
Noel Abellera <noel@noels-mbp.lan>
2018-01-13 11:02:02 -0800

    add user login and sign up

---
+++ eatme_recipe_app/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    # Provide the model instance to the form_for helper
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You have successfully signed up!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  # Implement Strong Params
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- eatme_recipe_app/config/routes.rb
+++ eatme_recipe_app/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#show', page: 'home'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+
 end
